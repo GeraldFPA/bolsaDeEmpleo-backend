5502678a82e5b04b9376eba18247cbd80b19d895
GeraldFPA <GERALD.PALADINO@ucr.ac.cr>
2025-05-10 20:29:28 -0600

    se configuraron los cors para trabajar con el front

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -24,7 +24,7 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed'],
             'phone_number' => ['required', 'string', 'max:15'],
         ]);
 
